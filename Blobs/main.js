e65900f5e6b7df0a255440b02182e97871fc9c86
Davi <reis.davi11010@gmail.com>
2021-02-14 13:06:17 -0300

    update

--- Blobs/main.js
+++ Blobs/main.js
@@ -15,7 +15,9 @@ class Blob {
 }
 
 let blobs = []
+let Percents = [1, 1, 1]
 function num(n) { blobs = []; for (let i=0; i<n; i++) blobs.push(new Blob()) }
+function rgbPercent(r,g,b) { Percents[0] = r; Percents[1] = g; Percents[2] = b;}
 
 createCanvas(900, 600)
 loadPixels()
@@ -26,8 +28,10 @@ loop = () => {
     for (let i=0; i<=pixels.length; i+=4) {
         let sum = 0
         for (let b of blobs) sum += 140*b.r/dist(position(i)[0], position(i)[1], b.x, b.y)
-        pixels[i] = pixels[i+1] = pixels[i+2] = 255
-        pixels[i+3] = sum
+        pixels[i] = sum*Percents[0]
+        pixels[i+1] = sum*Percents[1]
+        pixels[i+2] = sum*Percents[2]
+        pixels[i+3] = 255
     } 
     updatePixels()
     blobs.map(b => {b.update()})
